a3ca0e5ffa21cc445793bd9a20aa89e2dbb15a60
bRuttaZz <agrajpdasprivate@gmail.com>
2024-06-16 04:40:58 +0530

    name change: made it to github terms

--- src/build-src/lib/settings.js
+++ src/build-src/lib/settings.js
@@ -1,6 +1,6 @@
 
-const TEMPLATE_DIR = "./_source/build-src/templates" 
-const BLOGS_SRC_DIR = "./_source/writings/blogs"
+const TEMPLATE_DIR = "./src/build-src/templates" 
+const BLOGS_SRC_DIR = "./src/writings/blogs"
 
 BLOG_META_KEYS = ['title', 'shortDescription', 'longDescription', 'keyWords']
 
