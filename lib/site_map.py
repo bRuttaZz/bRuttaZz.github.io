666fc7a54bd0d8c7a9b7f205a40a0cd60f33dac6
bRuttaZz <agrajpdas@gmail.com>
2025-10-16 22:32:54 +0530

    + sitemap

--- lib/site_map.py
+++ lib/site_map.py
@@ -10,8 +10,6 @@ def generate_sitemap():
             file = file.relative_to(settings.dist_path)
             if file.name == "index.html":
                 file = file.parent
-            if file.suffix == ".html":
-                file = file.with_suffix("")
 
             url = urljoin(settings.og_base_url, str(file))
             _ = sitemap.write(url + "\n")
